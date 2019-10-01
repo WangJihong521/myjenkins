a0d49f55be127ea04ee1d11143a216d4eb9c3551
王基鸿 <1043429669@qq.com>
2019-10-02 06:26:25 +0800

    2222

--- src/main/java/com/wangjihong/www/controller/UserController.java
+++ src/main/java/com/wangjihong/www/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.wangjihong.www.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 	
-	@RequestMapping(name="/",method=RequestMethod.GET)
+	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
