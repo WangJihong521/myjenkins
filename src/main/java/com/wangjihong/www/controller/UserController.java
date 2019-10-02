4f75e1fed612bf35352935167e77a12e36e2aef0
王基鸿 <1043429669@qq.com>
2019-10-02 09:06:12 +0800

    3333

--- src/main/java/com/wangjihong/www/controller/UserController.java
+++ src/main/java/com/wangjihong/www/controller/UserController.java
@@ -1,10 +1,5 @@
 package com.wangjihong.www.controller;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -15,50 +10,4 @@ public class UserController {
 	public String index() {
 		return "index";
 	}
-	
-//	public static String sendGet(String urlNameString) {
-//		int a = 1/0;
-//        String result = "";
-//        BufferedReader in = null;
-//        try {
-//            URL realUrl = new URL(urlNameString);
-//            // 打开和URL之间的连接
-//            HttpURLConnection connection = (HttpURLConnection) realUrl.openConnection();
-//            // 设置通用的请求属性
-//            connection.setRequestProperty("accept", "*/*");
-//            connection.setRequestProperty("connection", "Keep-Alive");
-//            connection.setRequestProperty("user-agent",
-//                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-//            // 建立实际的连接
-//            connection.connect();
-//            // 获取所有响应头字段
-////            Map<String, List<String>> map = connection.getHeaderFields();
-//            // 遍历所有的响应头字段
-//          /*  for (String key : map.keySet()) {
-//                System.out.println(key + "--->" + map.get(key));
-//            }*/
-//            // 定义 BufferedReader输入流来读取URL的响应
-//            in = new BufferedReader(new InputStreamReader(
-//                    connection.getInputStream()));
-//            String line;
-//            while ((line = in.readLine()) != null) {
-//                result += line;
-//            }
-//            result = new String(result.getBytes(),"UTF-8");
-//        } catch (Exception e) {
-//            System.out.println("发送GET请求出现异常！" + e);
-//            e.printStackTrace();
-//        }
-//        // 使用finally块来关闭输入流
-//        finally {
-//            try {
-//                if (in != null) {
-//                    in.close();
-//                }
-//            } catch (Exception e2) {
-//                e2.printStackTrace();
-//            }
-//        }
-//        return result;
-//    }
 }
